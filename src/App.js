f3e5683cbaab005e9f44751623c3617371dd32f0
PranavJAIN12 <94856533+PranavJAIN12@users.noreply.github.com>
2024-04-28 17:52:05 +0530

    search component done

--- src/App.js
+++ src/App.js
@@ -8,34 +8,51 @@ import RecipeItem from './Components/RecipeItem';
 
 function App() {
   const [recipes, setRecipes] = useState([]);
+  const[search, setSearch] = useState("")
+
   const api = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
 
-  useEffect(() => {
-    const searchRecipe = async () => {
-      try {
-        const response = await axios.get(api);
-        setRecipes(response.data.meals); // Assuming 'meals' holds the array of recipes
-        console.log(response.data);
-      } catch (error) {
-        console.error('Error fetching recipes:', error);
-      }
-    };
+  const searchRecipe = async () => {
+    try {
+      const response = await axios.get(api + search);
+      setRecipes(response.data.meals); 
+      console.log(response.data);
+    } catch (error) {
+      console.error('Error fetching recipes:', error);
+    }
+  };
 
+  useEffect(() => {
     searchRecipe();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    searchRecipe();
+  };
+  const handleChange=(e)=>{
+    setSearch(e.target.value)
+}
 
   return (
     <>
       <Navbar />
       <HomePage recipes={recipes} />
-      <SearchBar />
+      <SearchBar handleSubmit={handleSubmit} 
+        search={search}
+        handleChange={handleChange}
+      />
       <div className='d-flex justify-content-between flex-wrap recipe-sec'>
-        {recipes.map(recipe => (
-          <RecipeItem key={recipe.idMeal} recipe={recipe}
-          imageUrl={recipe.strMealThumb}
-          title = {recipe.strMeal} 
-          category = {recipe.strCategory}
-          area = {recipe.strArea} />
+        {recipes.map((recipe) => (
+          <RecipeItem
+            key={recipe.idMeal}
+            recipe={recipe}
+            imageUrl={recipe.strMealThumb}
+            title={recipe.strMeal}
+            category={recipe.strCategory}
+            area={recipe.strArea}
+          />
         ))}
       </div>
     </>
--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,25 +1,19 @@
-import React,{useState} from 'react'
+import React from 'react'
 
-export default function SearchBar() {
-    const[search, setSearch] = useState("")
+export default function SearchBar(props) {
 
-    const handleSubmit=(e)=>{
-        console.log("button clicked")
-        e.preventDefault();
-    }
+    
 
-    const handleChange=(e)=>{
-        setSearch(e.target.value)
-    }
+    
 
   return (
     <div className='searh container text-center'>
     <h2 className='text-center'>Our Recipe App</h2>
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={props.handleSubmit}>
       
       <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Search for a food item..."
-        value={search}
-        onChange={handleChange}></input>
+        value={props.search}
+        onChange={props.handleChange}></input>
       <button className="btn btn-primary"type="submit">Search</button>
     </form>
       
